2efc03a65f0c8ab1b635827cc5b64be913a9b8bb
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-11-08 13:24:43 +0100

    journal-upload: use mfree in one more place

--- src/journal-remote/journal-upload.c
+++ src/journal-remote/journal-upload.c
@@ -279,9 +279,7 @@ int start_upload(Uploader *u,
         } else {
                 /* truncate the potential old error message */
                 u->error[0] = '\0';
-
-                free(u->answer);
-                u->answer = 0;
+                u->answer = mfree(u->answer);
         }
 
         /* upload to this place */
