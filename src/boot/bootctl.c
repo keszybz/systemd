f2d10d8b84caa5e72e76775bafa3a42e42de42e5
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2016-07-24 14:12:58 -0400

    Add enable_disable() helper

--- src/boot/bootctl.c
+++ src/boot/bootctl.c
@@ -1061,7 +1061,7 @@ static int bootctl_main(int argc, char*argv[]) {
                         if (r < 0)
                                 log_warning_errno(r, "Failed to query secure boot status: %m");
                         else
-                                printf("  Secure Boot: %s\n", r ? "enabled" : "disabled");
+                                printf("  Secure Boot: %sd\n", enable_disable(r));
 
                         r = is_efi_secure_boot_setup_mode();
                         if (r < 0)
